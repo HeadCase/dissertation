3762c923ed8cca0e92f77e3c502f455fd9db685b
fantasmo <gheadley27@gmail.com>
2020-06-29 19:21:08 +0100

    Initial commit for dissertation repo

---
+++ chap1/MCMC-example.py
@@ -0,0 +1,12 @@
+"""
+File: MCMC-example.py
+Author: G. W. Headley
+Email: mm19gwh@leeds.ac.uk
+Github: https://github.com/HeadCase
+Description: Python code to produce examples for dissertation chapter on
+Markov chain Monte Carlo
+"""
+
+import networkx as nx
+
+G = nx.Graph()
