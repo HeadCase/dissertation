1e57d51dccbf1943fb209d0cc3a93027601054b2
fantasmo <gheadley27@gmail.com>
2020-07-01 22:34:04 +0100

    Instantiated example file

---
+++ chap2/toy-example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+File: toy-example.py
+Author: G. W. Headley
+Email: mm19gwh@leeds.ac.uk
+Github: https://github.com/HeadCase
+Description: Source code for example MCMC/redistricting example in
+introductory MCMC chapter
+"""
+
+foo!
