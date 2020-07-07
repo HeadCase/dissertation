289e3a6363f1a2e0cda92e3180adff1ffd4f9106
fantasmo <gheadley27@gmail.com>
2020-07-07 21:25:53 +0100

    Proposal bug appears to be in propose.py not here

--- chap2/chain.py
+++ chap2/chain.py
@@ -2,9 +2,9 @@
 
 # Imports
 from copy import deepcopy as dc
-from random import randint as rint
 from random import uniform
 from propose import candidates
+from propose import distr_threshold
 
 
 # Markov Chain Simulation
@@ -18,35 +18,33 @@ def chain(graph, n):
     plans = [graph]
     while i < n:
         # Candidates reset to null at start of each loop
-        candNodes = []
-        sourceNode = 0
+        # candNodes = []
+        # sourceNode = 0
+        print(id(graph))
 
-        # Get proposal node for potential district change. candidates()
-        # function can return empty list (node that has no neighbours from
-        # other districts), so we loop until we get a non-empty list
-        # while candNodes == []:
-        sourceNode, candNodes = candidates(graph)
-        if len(candNodes) > 1:
-            pick = rint(0, len(candNodes) - 1)
-            proposal = candNodes[pick]
-        else:
-            proposal = candNodes[0]
+        sourceNode, proposalNode = candidates(graph)
+        srcDistr = graph.nodes[sourceNode]["dist"]
+        print(
+            "Validity src: {}, Validity prp: {}".format(
+                distr_threshold(sourceNode, graph), distr_threshold(proposalNode, graph)
+            )
+        )
 
         score_source = graph.nodes[sourceNode]["pop"]
         q_source = 1 / len(graph.adj[sourceNode])
-        score_prop = graph.nodes[proposal]["pop"]
-        q_prop = 1 / len(graph.adj[proposal])
+        score_prop = graph.nodes[proposalNode]["pop"]
+        q_prop = 1 / len(graph.adj[proposalNode])
 
         alpha = min(1, (score_prop / score_source) * (q_prop / q_source))
         beta = uniform(0.5, 1)
 
         if alpha > beta:
             update = dc(graph)
-            update.nodes[proposal]["dist"] = update.nodes[sourceNode]["dist"]
+            update.nodes[proposalNode]["dist"] = srcDistr
             graph = update
             plans.append(graph)
 
-        print("End of iteration {}".format(i))
         i += 1
+        # print("End of iteration {}".format(i))
 
     return plans
