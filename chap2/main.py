f63228c04584aea39ca950ca5b6071c419c86d9f
fantasmo <gheadley27@gmail.com>
2020-07-08 16:44:21 +0100

    No major changes to main

--- chap2/main.py
+++ chap2/main.py
@@ -4,6 +4,8 @@
 # Imports
 from init import init_graph
 from reject import reject_islands
+from reject import reject_by_pop
+from copy import deepcopy as dc
 from draw import draw
 from chain import chain
 
@@ -13,20 +15,61 @@ def main():
 
     S = init_graph()
 
-    plans = chain(S, 2)
-    reject_islands(plans)
-    # print(len(plans))
+    # plans = [S]
+    # S2 = dc(S)
+    # S2.nodes[26]["dist"] = 1
+    # plans.append(S2)
+    plans = chain(S, 20000)
+    contiguous, reject = reject_islands(plans)
+    clean, reject2 = reject_by_pop(contiguous)
+    reject.extend(reject2)
+
+    for graph in clean:
+        # graph = clean[index]
+        index = 1
+        print("Plan {}".format(index))
+        print("---------------------")
+        for i in range(1, 5):
+            nodes_in_distr = [
+                node for node, attrs in graph.nodes(data=True) if attrs["dist"] == i
+            ]
+            total_pop = 0
+            for node in nodes_in_distr:
+                total_pop += graph.nodes[node]["pop"]
+            print("District {}: total population - {}".format(i, total_pop))
+        print("---------------------")
+        index += 1
+
+    print("{} raw plans".format(len(plans)))
+    print("Kept {} clean plans".format(len(clean)))
+    print("Rejected {} non-contiguous or malapportioned plans".format(len(reject)))
+
+    count = 1
+    for i in clean:
+        draw(i, "clean-plans{}".format(count))
+        print("------ Clean plan {} drawn ------".format(count))
+        count += 1
 
     # count = 1
-    # for i in plans:
-    #     draw(i, "refactor-S-plan{}".format(count))
-    #     print("------ Iteration {} drawn ------".format(count))
+    # for i in reject:
+    #     draw(i, "reject-plans{}".format(count))
+    #     print("------ Reject {} drawn ------".format(count))
     #     count += 1
 
     # draw(S, "refactor-S-edit")
     # print(candidates(S))
     # draw(S, "proposal-debug")
+    # draw(S2, "island-test")
 
 
 if __name__ == "__main__":
     main()
+
+
+############
+# Snippets #
+############
+
+# nodes_in_distr = [
+#     node for node, attrs in graph.nodes(data=True) if attrs["dist"] == i
+# ]
