18023cab09c1168765872fd3b4e6e9e08d76449c
fantasmo <gheadley27@gmail.com>
2020-07-13 15:51:11 +0100

    Added breadth-first search functions for contiguity tests

--- chap2/utils.py
+++ chap2/utils.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 """ Source code of utility functions used across the application """
 
+from collections import deque
+
+
+def distr_nodes(distr, graph):
+    """ Accept a district from a graph and parse, producing a list of all
+    nodes in that district """
+
+    nodes_in_distr = [
+        node for node, attrs in graph.nodes(data=True) if attrs["distr"] == distr
+    ]
+
+    return nodes_in_distr
+
 
 def distr_count(graph):
     """ Function to retrieve the number of districts present in a supplied
@@ -18,11 +31,54 @@ def distr_count(graph):
 def distr_pop(distr, graph):
     """ Tabulates the number of residents in a district """
 
-    nodes_in_distr = [
-        node for node, attrs in graph.nodes(data=True) if attrs["distr"] == distr
-    ]
+    nodes_in_distr = distr_nodes(distr, graph)
     total_pop = 0
     for node in nodes_in_distr:
         total_pop += graph.nodes[node]["pop"]
 
     return total_pop
+
+
+def contig_distr(distr, graph):
+    """ Deploys breadth-first search algorithm to determine if supplied
+    district is contiguous. Returns boolean result """
+
+    # Produce list of nodes in the district in question
+    d_nodes = distr_nodes(distr, graph)
+    init_node = d_nodes[0]
+
+    # Send required arguments to breadth-first search (BFS) algorithm to get a
+    # list of contiguous nodes in the district
+    contig_nodes = bfs(init_node, distr, distr_nodes, graph)
+
+    # While the code above initialises with the first node in the district, it
+    # doesn't really matter. If the BFS returns any list of contiguous nodes
+    # that is not exactly the same as the full list of nodes in the district,
+    # the district cannot be contiguous
+    if set(d_nodes) == set(contig_nodes):
+        return True
+    else:
+        return False
+
+
+def bfs(node, distr, d_nodes, graph):
+    """ Breadth-first search (BFS) for traversing the full set of nodes, from
+    the supplied district, which are contiguous. """
+
+    # BFS is implemented using a queue and visit pair of lists. The queue
+    # tracks nodes that need to be explored. Once explored, they are removed
+    # from the queue and appended to the visited list. A while loop keeps the
+    # search going until the queue is empty.
+    queue = deque([node])
+    visited = [node]
+
+    while queue:
+        curr_node = queue.popleft()
+        nbors = list(graph.neighbors(curr_node))
+        d_nbors = [item for item in nbors if graph.nodes[item]["distr"] == distr]
+        for n in d_nbors:
+            if n not in visited:
+                queue.append(n)
+                visited.append(n)
+
+    return visited
