9a6b87853bbb231b44b3d9a1d711c85f9fe90ae3
fantasmo <gheadley27@gmail.com>
2020-07-13 22:15:42 +0100

    Created election source file

---
+++ chap2/election.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+""" Source code for calculating voting totals and statistics"""
+
+
+def election(plan):
+    """ Accept a districting plan and computes results of an election under that plan """
+    pass
