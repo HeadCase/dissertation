fbcf1d70d1c85ca00c08cbe843edf565a1aa6a10
fantasmo <gheadley27@gmail.com>
2020-07-13 15:50:32 +0100

    Separated population and contiguity scoring into separate functions, defined overall plan scoring function

--- chap2/score.py
+++ chap2/score.py
@@ -3,6 +3,7 @@
 
 from utils import distr_count
 from utils import distr_pop
+from utils import contig_distr
 from math import exp as e
 from statistics import pstdev
 
@@ -12,6 +13,23 @@ def score_plan(plan, const=0.025):
     it for apportionment equality. The score function takes the form of an
     energy function using the standard deviation of the population of each
     district """
+
+    # Get population score parameter
+    pop_param = score_pop(plan)
+
+    # Get contiguity score parameter
+    contig_param = score_contig(plan)
+
+    # Energy function with constant to adjust 'strength' of scoring
+    score = e(-const * pop_param * contig_param)
+
+    return score
+
+
+def score_pop(plan):
+    """ Score a supplied plan for its apportionment equality. That is, check
+    the population deviation of each district from the ideal average. Returns
+    a standard deviation value """
 
     # Retrieve the number of districts in the given plan and create empty list
     # for population values from each district
@@ -25,10 +43,31 @@ def score_plan(plan, const=0.025):
         # Tabulate population of each district and append to list
         distr_pops.append(distr_pop(i, plan))
 
-    # Standard library standard deviation function for iterable input
+    # pstdev is the standard library standard deviation function for an
+    # iterable input
     std_dev = pstdev(distr_pops)
 
-    # Energy function with constant to adjust 'strength' of scoring
-    score = e(-const * std_dev)
+    return std_dev
+
+
+def score_contig(plan):
+    """ Scores a supplied plan based on the number of contiguous districts
+    present. If all are contiguous, returns 1; else, returns 0.5 ^ x, where x
+    is the number of non-contiguous districts """
+
+    num_distrs = distr_count(plan)
+
+    # Build up a list of results for the contiguity test
+    contig_list = []
+    for i in range(1, num_distrs + 1):
+        contig_list.append(contig_distr(i, plan))
+
+    # Count the number of disconnected districts
+    num_discon = 0
+    for item in contig_list:
+        if not item:
+            num_discon += 1
+
+    score = 0.5 ** num_discon
 
     return score
