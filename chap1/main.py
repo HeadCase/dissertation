d03ab5c1dc4b13b2a811ac58817b438a87f259fb
fantasmo <gheadley27@gmail.com>
2020-07-14 22:08:53 +0100

    Main function

---
+++ chap1/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+""" Main function """
+
+# Imports
+# from init import init_graph
+
+# import sys
+# import numpy
+# numpy.set_printoptions(threshold=sys.maxsize)
+
+
+def main():
+    """ Main function """
+    pass
+
+
+if __name__ == "__main__":
+    main()
