a4a162526a25d864433409013ca601d88d13aba3
fantasmo <gheadley27@gmail.com>
2020-08-06 16:33:16 +0100

    Even messier main function

--- chap2/main.py
+++ chap2/main.py
@@ -8,6 +8,7 @@ from election import calc_winner
 from reject import reject_islands
 from reject import reject_by_pop
 from draw import draw
+from draw import single_plot_params
 from chain import chain
 
 from statistics import pstdev
@@ -38,67 +39,83 @@ def main():
     """ Main function """
 
     S = init_graph()
+    labs, sizes, colours = single_plot_params(S, "pop", "green")
+    pos = S.graph["position"]
+    nlist = list(S.nodes)
+
+    nx.draw(
+        S,
+        pos,
+        labels=labs,
+        node_list=nlist,
+        node_color=colours,
+        node_size=sizes,
+        font_size=50,
+        node_shape="s",
+    )
+    plt.show()
+
     # results = election(S)
     # winners = calc_winner(results)
     # for distr in winners.items():
     #     print(distr)
 
-    plans = chain(S, 15000, 0.25)
+    # plans = chain(S, 15000, 0.25)
 
-    apport, reject = reject_by_pop(plans)
-    contiguous, reject2 = reject_islands(apport)
-    reject.extend(reject2)
+    # apport, reject = reject_by_pop(plans)
+    # contiguous, reject2 = reject_islands(apport)
+    # reject.extend(reject2)
 
-    print("{} raw plans".format(len(plans)))
-    print("Kept {} clean plans".format(len(contiguous)))
-    print(
-        "Rejected {} malapportioned plans and {} non-contiguous plans".format(
-            len(reject), len(reject2)
-        )
-    )
+    # print("{} raw plans".format(len(plans)))
+    # print("Kept {} clean plans".format(len(contiguous)))
+    # print(
+    #     "Rejected {} malapportioned plans and {} non-contiguous plans".format(
+    #         len(reject), len(reject2)
+    #     )
+    # )
 
-    count = 1
-    for i in contiguous:
-        # results = election(i)
-        # winners = calc_winner(results)
-        # print("Results for plan {}".format(count))
-        # for distr in winners.items():
-        #     print(distr)
-        count += 1
+    # count = 1
+    # for i in contiguous:
+    #     # results = election(i)
+    #     # winners = calc_winner(results)
+    #     # print("Results for plan {}".format(count))
+    #     # for distr in winners.items():
+    #     #     print(distr)
+    #     count += 1
 
-        pos = i.graph["position"]
-        nlist = list(i.nodes)
-        size = []
-        colour = []
+    #     pos = i.graph["position"]
+    #     nlist = list(i.nodes)
+    #     size = []
+    #     colour = []
 
-        for n in nlist:
-            size.append((i.nodes[n]["pop"] ** 3) / 3)
-            if i.nodes[n]["distr"] == 1:
-                colour.append("#EEB653")
-            elif i.nodes[n]["distr"] == 2:
-                colour.append("#CE477B")
-            elif i.nodes[n]["distr"] == 3:
-                colour.append("#40649D")
-            else:
-                colour.append("#96D84B")
+    #     for n in nlist:
+    #         size.append((i.nodes[n]["pop"] ** 3) / 3)
+    #         if i.nodes[n]["distr"] == 1:
+    #             colour.append("#EEB653")
+    #         elif i.nodes[n]["distr"] == 2:
+    #             colour.append("#CE477B")
+    #         elif i.nodes[n]["distr"] == 3:
+    #             colour.append("#40649D")
+    #         else:
+    #             colour.append("#96D84B")
 
-        labels = {}
-        for n in nlist:
-            labels[n] = i.nodes[n]["pop"]
+    #     labels = {}
+    #     for n in nlist:
+    #         labels[n] = i.nodes[n]["pop"]
 
-        nx.draw(
-            i,
-            pos,
-            labels=labels,
-            font_size=60,
-            node_list=nlist,
-            node_size=size,
-            node_shape="s",
-            node_color=colour,
-            linewidths=4,
-            width=4,
-        )
-        plt.show()
+    #     nx.draw(
+    #         i,
+    #         pos,
+    #         labels=labels,
+    #         font_size=60,
+    #         node_list=nlist,
+    #         node_size=size,
+    #         node_shape="s",
+    #         node_color=colour,
+    #         linewidths=4,
+    #         width=4,
+    #     )
+    #     plt.show()
 
     # plans, _ = reject_by_pop(plans)
     # plans, _ = reject_islands(plans)
