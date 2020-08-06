8132fe9cc8846f2230f97cbefd60b650e54cc6b3
fantasmo <gheadley27@gmail.com>
2020-08-06 18:26:00 +0100

    Moved execution of plots out of main.py into this file

---
+++ chap2/plots.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+""" Main function """
+
+# Imports
+from init import init_graph
+from draw import single_plot_params
+from draw import distr_plot_params
+
+import networkx as nx
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import matplotlib.font_manager as font_manager
+
+mpl.rcParams["font.family"] = ["sans-serif"]  # fancy fonts
+mpl.rcParams["font.sans-serif"] = ["Source Sans Pro"]
+
+
+def main():
+    """ Main function """
+
+    # col_map = colour_scale(all=True)
+    # print(col_map[1])
+    S = init_graph()
+    print(S.nodes.data("sqre_marg"))
+    labs, sizes, colours = single_plot_params(S, "sqre_marg", "purple")
+    pos = S.graph["position"]
+    nlist = list(S.nodes)
+    colours = [marg[1] for marg in S.nodes.data("sqre_marg")]
+
+    nx.draw(
+        S,
+        pos,
+        labels=labs,
+        node_list=nlist,
+        node_color=colours,
+        node_size=sizes,
+        font_size=56,
+        node_shape="o",
+        cmap=plt.cm.RdYlGn,
+    )
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
