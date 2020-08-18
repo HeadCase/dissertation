1cc8e13a7d1882d25805b2b3a080d68679b5d448
fantasmo <gheadley27@gmail.com>
2020-08-18 11:27:11 +0100

    successful 4mil run params

--- redist/main.py
+++ redist/main.py
@@ -41,10 +41,10 @@ numpy.set_printoptions(threshold=sys.maxsize)
 def main():
     """ Main function """
 
-    basename = "reparam-const00pt25-ban-ncontig-variance"
+    basename = "4mil-const00pt25-ban-ncontig-variance"
     S = init_expanded()
 
-    plans = chain(S, 40000, 0.0025, "logs/{}".format(basename))
+    plans = chain(S, 4000000, 0.0025, "logs/{}".format(basename))
 
     with open("logs/{}.txt".format(basename), "a+") as f:
         sys.stdout = f
